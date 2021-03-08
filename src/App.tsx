ac872d1ad6a0560fef3a4e9228ff52d87967ab25
widjuu <4yvak08@mail.ru>
2021-03-08 21:54:05 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+//components
+import { Calc } from "./components/Calc";
+import { Switch } from "./components/Switch";
+
+import "./css/style.css";
+
+export const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Switch />
+      <Calc />
     </div>
   );
-}
-
-export default App;
+};
